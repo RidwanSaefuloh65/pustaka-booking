ca47adad709c068db623564c021b4ca4f70475b0
ridwansaefuloh <ridwansaefuloh65@gmail.com>
2022-03-29 02:42:25 -0700

    membuat folder pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama Saya Ridwan Saefuloh
+            Saya tinggal di daerah Kedoya utara
+            olahraga yang saya sukai adalah 
+            berenang";
+    }
+}
