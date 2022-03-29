acbab5cee90cf4a06c670da65b01aefcc43c0e8d
ridwansaefuloh <ridwansaefuloh65@gmail.com>
2022-03-29 04:58:21 -0700

    Pert2-Merubah-latihan1-modellatihan1

--- application/models/Model_latihan1.php
+++ application/models/Model_latihan1.php
@@ -1,17 +1,16 @@
 <?php
-class Latihan1 extends CI_Controller
-{   
-    public function index()
-    {
-        echo "Selamat Datang.. selamat belajar web Progamming";
-        //$this->load->view('view-latihan1');
+class Model_latihan1 extends CI_Model
+{
+ //membuat variable untuk menampung nilai
+ public $nilai1, $nilai2, $hasil;
+ //method penjumlahan
+ public function jumlah($n1 = null, $n2 = null)
+ {
+    $this->nilai1 = $n1;
+    $this->nilai2 = $n2;
+    $this->hasil = $this->nilai1 + $this->nilai2;
+    return $this->hasil;
     }
+   }
 
-    public function penjumlahan($n1, $n2)
-    {
-    $this->load->model('Model_latihan1');
-    $hasil = $this->Model_latihan1->jumlah($n1, $n2);
-    echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
-.$hasil;
-    }
-}
+   ?>
